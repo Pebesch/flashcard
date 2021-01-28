870c234d034a1b8966a7faebfd24284cb29495b8
Peter Schmucki <pebs@gmx.ch>
2021-01-28 16:25:40 +0100

    Added Controller input validation and html result

--- src/main/java/ch/fhnw/webfr/flashcard/domain/Questionnaire.java
+++ src/main/java/ch/fhnw/webfr/flashcard/domain/Questionnaire.java
@@ -1,7 +1,5 @@
 package ch.fhnw.webfr.flashcard.domain;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 
 import org.springframework.data.annotation.Id;
--- src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -43,7 +45,11 @@ public class QuestionnaireController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String create(Questionnaire questionnaire) {
+    public String create(@Valid Questionnaire questionnaire, BindingResult bindingResult, Model model) { 
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("questionnaire", questionnaire);
+            return "questionnaires/create";
+        }
         Questionnaire q = questionnaireRepository.save(questionnaire);
         return "redirect:questionnaires/" + q.getId();
     }
